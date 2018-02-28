98589047ebcfaef8161a945a95fe9c852d9963b0
Shubham Desale <shubham.desale@gmail.com>
2018-02-28 19:43:43 +0530

    Limit image heights using background image, card size becomes same

--- src/components/presentation/SelectedUniversities.js
+++ src/components/presentation/SelectedUniversities.js
@@ -3,6 +3,15 @@ import React, { Component } from 'react';
 const numberWithCommas = (x) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+const getImageStyle = url => ({
+  backgroundSize: 'cover',
+  backgroundImage: 'url('+url+')',
+  height: '230px',
+  width: '100%',
+  display: 'block',
+  borderRadius: '4px',
+  backgroundPosition: 'center center'
+});
 
 export default (props) => {
   const { selectedUniversities = [], savedUniversities = [] } = props;
@@ -14,21 +23,21 @@ export default (props) => {
     const should_have_row = index%2;
     return(
       <div key={_id} className='col-md-6' style={style.card}>
-        <i onClick={props.removeUniversityFromSelected.bind(this, _id)} style={{size: "20"}} style={style.closeButton} className="fa fa-window-close pull-right"></i>
+        <i onClick={props.removeUniversityFromSelected.bind(this, _id)} style={style.closeButton} className="fa fa-window-close pull-right"></i>
         <div className="thumbnail">
-          <img style={style.img} className="card-img-top" src={ imgURL } alt="university_img"/>
-            <div className="caption">
+          <div style={getImageStyle(imgURL)} className="card-img-top"></div>
+          <div className="caption">
             <h3>{ school_name }</h3>
-              <p>{ description }</p>
-                <small className="text-muted">Ranked #{ ranking } among universities in the US.</small><br/>
-                <small className="text-muted">Annual tuition $ {numberWithCommas( tuition )}</small><br/>
-                <small className="text-muted">Acceptance rate: { acceptance_rate }%</small>
-              <p>
-              {(is_already_saved)?
-                <button onClick={props.deleteSchool.bind(this, _id)} className="btn btn-primary" role="button"> Saved <i className="fa fa-heart"></i></button>:
-                <button onClick={props.saveSchool.bind(this, university)} className="btn btn-primary" role="button"> Save </button>}
-              </p>
-           </div>
+            <p>{ description }</p>
+              <small className="text-muted">Ranked #{ ranking } among universities in the US.</small><br/>
+              <small className="text-muted">Annual tuition $ {numberWithCommas( tuition )}</small><br/>
+              <small className="text-muted">Acceptance rate: { acceptance_rate }%</small>
+            <p>
+            {(is_already_saved)?
+              <button onClick={props.deleteSchool.bind(this, _id)} className="btn btn-primary" role="button"> Saved <i className="fa fa-heart"></i></button>:
+              <button onClick={props.saveSchool.bind(this, university)} className="btn btn-primary" role="button"> Save </button>}
+            </p>
+          </div>
          </div>
        </div>
     );
@@ -48,14 +57,13 @@ const style = {
     minHeight: '550px',
   },
   img: {
-    WebkitBoxFlex: "0",
-    WebkitFlex: "0 0 auto",
-    msFlex: "0 0 auto",
-    flex: "0 0 auto"
+    width: '100%'
   },
   closeButton: {
     position: 'absolute',
-    top: '-3px',
-    right: '11px'
+    top: '-5px',
+    fontSize: '20px',
+    right: '12px',
+    background: 'white'
   }
 }
