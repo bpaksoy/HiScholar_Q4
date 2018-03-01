2a89a01354a35dccd275394b9ebe72438875e1d8
bpaksoy <paksoybanu83@gmail.com>
2018-03-01 12:37:28 -0500

    display tweets added

--- routes/twitter-routes.js
+++ routes/twitter-routes.js
@@ -6,7 +6,6 @@ const Twitter = require("twit");
 
 router.get("/tweets", (req, res, next) => {
 
-console.log("HI MOM")
   const twitter = new Twitter({
     consumer_key:keys.twitter.consumerKey,
     consumer_secret: keys.twitter.consumerSecret,
@@ -14,14 +13,14 @@ console.log("HI MOM")
     access_token_secret: keys.twitter.accessTokenSecret,
     timeout_ms: 5000,
   });
-  let twitter_handle = "hamiltonhall";
+  let twitter_handle = "MeetNYU";
 
   //console.log("twitter", twitter);
   //gets tweets of specific user by querying Twitter Handle
   twitter.get('statuses/user_timeline', { screen_name: twitter_handle, count: 1 },
   function (err, data, response) {
 
-      console.log(JSON.stringify(data[0].text));
+      //console.log(JSON.stringify(data[0].text));
     //  console.log(school);
       res.send(data);
     })
