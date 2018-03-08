41d377f289ea5de0fb77f065e800df31591838e2
Shubham Desale <shubham.desale@gmail.com>
2018-03-08 23:13:38 +0530

    Add code for multiple tweet search

--- routes/twitter-routes.js
+++ routes/twitter-routes.js
@@ -15,12 +15,40 @@ router.get("/tweets", (req, res, next) => {
     timeout_ms: 5000,
   });
 
+  let all_tweets = []
+  const all_handles = [] //
+
+  // all_handles.forEach((handle) => {
+  //   twitter.get('search/tweets', { q: handle+' application', count: 10 }, function (err, data, response) {
+  //     all_tweets.push(data);
+  //     res.send(data);
+  //   })
+  // })
 //gets tweets of specific user by querying Twitter Handle
   //let twitter_handle = "MeetNYU";
   // twitter.get('statuses/user_timeline', { screen_name: twitter_handle, count: 3 },
   //   function (err, data, response) {
   //       res.send(data);
   //  })
+
+  // const all_promises = [];
+  // all_handles.forEach((handle) => {
+
+  //   const p = new Promise((resolve, reject) => {
+  //     twitter.get('search/tweets', { q: handle+' application', count: 10 }, function (err, data, response) {
+  //       if (err) {
+  //         reject()
+  //       }
+  //       resolve(data);
+  //     })
+  //   })
+  //   all_promises.push(p);
+  // })
+
+  // Promise.all(all_promises).then((all_tweets) => {
+  //   console.log(all_tweets);
+  // })
+
 
   twitter.get('search/tweets', { q: 'MeetNYU application', count: 10 }, function (err, data, response) {
     console.log(data);
